bdf6083d82d2c73b1f2fbf4c52fb3127265113e2
shephali09 <hiremathshephali9@gmail.com>
2023-03-19 19:35:06 +0530

    Add: Print Welcome Message

---
+++ src/com/bridgelabz/linecomparisonproblem/LineComparisonProblem.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.linecomparisonproblem;
+
+public class LineComparisonProblem {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Line Comparison Computation Program!");
+	}
+}
